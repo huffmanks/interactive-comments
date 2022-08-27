a05ee1007df9a09747baf0f517279da5a57aed50
huffmanks <huffmanks2@gmail.com>
2022-08-27 17:18:05 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+const App = () => {
+    return (
+        <div>
+            <div>hello</div>
+            <div className='text-[11px] text-center'>
+                <span>Challenge by </span>
+                <a style={{ color: 'var(--moderate-blue)' }} href='https://www.frontendmentor.io?ref=challenge'>
+                    Frontend Mentor
+                </a>
+                . <span>Coded by </span>
+                <a style={{ color: 'var(--moderate-blue)' }} href='https://huffmanks.com'>
+                    Kevin Huffman
+                </a>
+                .
+            </div>
+        </div>
+    )
+}
+
+export default App
